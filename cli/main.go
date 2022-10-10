1540eba20926954da1a6d8fbebd304b9889605f5
Jeff Palm <jeff@jeffpalm.com>
2022-10-10 07:38:52 -0400

    update Mon Oct 10 07:38:52 EDT 2022

--- cli/main.go
+++ cli/main.go
@@ -3,27 +3,21 @@ package cli
 import (
 	"context"
 
-	"github.com/spudtrooper/goutil/flags"
 	"github.com/spudtrooper/minimalcli/handler"
+	flag "github.com/spudtrooper/minimalcli/handler"
 	"github.com/spudtrooper/resy/api"
 	"github.com/spudtrooper/resy/handlers"
 )
 
-var (
-	term         = flags.String("term", "search term")
-	debugBody    = flags.Bool("debug_body", "debug body")
-	debugPayload = flags.Bool("debug_payload", "debug payload")
-	urlSlug      = flags.String("url_slug", "url slug")
-	location     = flags.String("location", "location")
-)
-
 func Main(ctx context.Context) error {
+	flag.String("term", "", "search term")
+	flag.Bool("debug_body", false, "debug body")
+	flag.Bool("debug_payload", false, "debug payload")
+	flag.String("url_slug", "", "url slug")
+	flag.String("location", "", "location")
+
 	adp := handler.NewCLIAdapter()
-	adp.BindStringFlag("term", term)
-	adp.BindBoolFlag("debug_body", debugBody)
-	adp.BindBoolFlag("debug_payload", debugPayload)
-	adp.BindStringFlag("url_slug", urlSlug)
-	adp.BindStringFlag("location", location)
+	adp.BindToGlobalFlags()
 
 	client, err := api.NewClientFromFlags()
 	if err != nil {
