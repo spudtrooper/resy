0ce066b7e356f3519cf87c16ecd79b1c655219e6
Jeff Palm <jeff@jeffpalm.com>
2022-10-15 09:41:20 -0400

    update Sat Oct 15 09:41:20 EDT 2022

--- cli/main.go
+++ cli/main.go
@@ -11,8 +11,6 @@ import (
 
 func Main(ctx context.Context) error {
 	flag.String("term", "", "search term")
-	flag.Bool("debug_body", false, "debug body")
-	flag.Bool("debug_payload", false, "debug payload")
 	flag.String("url_slug", "", "url slug")
 	flag.String("location", "", "location")
 	flag.Int("venue_id", 0, "venue ID")
